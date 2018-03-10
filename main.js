8e6d85c9fa521c0994efa6600d850d718226c261
jlalcantara <36500066+jlalcantara@users.noreply.github.com>
2018-03-10 11:10:48 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,78 +1,104 @@
-    const readline = require('readline');
-    const {log, biglog,errorlog,colorize} = require('./out')
-    const cmds = require('./cmds');
-    const model = require('./model');
+
+const readline = require('readline');
+const {log, biglog, errorlog, colorize} = require("./out");
+const cmds = require("./cmds");
 
 
-    /**
-     * Mensaje inicial
-     */
-biglog('CORE quiz', 'green');
 
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-        prompt: colorize ("quiz>", 'blue'),
-        completer(line) {
-        const completions = 'help h test delete quit q add list play p test show edit credits'.split(' ');
-        const hits = completions.filter((c) => c.startsWith(line));
-        // show all completions if none found
-        return [hits.length ? hits : completions, line];
-    }
-    });
 
-    rl.prompt();
 
-    rl.on('line', (line) => {
+//Mensaje inicial
 
-        let args = line.split(" ");
-        let cmd = args[0].toLocaleLowerCase().trim();
+biglog('CORE Quiz', 'green');
 
-        switch (cmd) {
-            case '':
-                rl.prompt();
-                break;
-            case 'h':
-            case 'help':
-                cmds.helpCmd(rl);
-                break;
-            case'q':
-            case'quit':
-                cmds.quitCmd(rl);
-                break;
-            case 'add':
-                cmds.addCmd(rl);
-                break;
-            case 'list':
-                cmds.listCmd(rl);
-                break;
-            case 'test':
-                cmds.testCmd(rl,args[1]);
-                break;
-            case 'play':
-            case 'p':
-                cmds.playCmd(rl);
-                break;
-            case 'show':
-                cmds.showCmd(rl,args[1]);
-                break;
-            case 'delete':
-                cmds.deleteCmd(rl, args[1]);
-                break;
-            case 'edit':
-                cmds.editCmd(rl, args[1]);
-                break;
-            case 'credits':
-                cmds.creditsCmd(rl);
-                break;
-            default:
-                console.log(`Comando desconocido '${colorize(cmd, 'red')}'`)
-                console.log('Use help para ver todos los comandos desconocidos');
-                rl.prompt();
-                break;
 
-    }
-    }).on('close', () => {
-        console.log('Adios!');
-    process.exit(0);
-    });
+
+const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+		prompt: colorize("quiz >", 'white'),
+
+		completer: (line) => {
+			const completions = 'h help add delete edit list test p play credits q quit'.split(' ');
+			const hits = completions.filter((c) => c.startsWith(line));
+			// show all completions if none found
+			return [hits.length ? hits : completions, line];
+	}
+});
+
+rl.prompt();
+
+rl
+.on('line', (line) => {
+
+		let args = line.split(" ");
+		let cmd = args[0].toLowerCase().trim();
+
+
+		switch (cmd) {
+
+	case '':
+		rl.prompt(); 
+		break;
+
+	case 'quit':
+	case 'q':
+		cmds.quitCmd(rl);
+		break;
+
+
+	case 'help':
+	case 'h':
+		cmds.helpCmd(rl);
+		break;
+
+	
+	case 'add':
+		cmds.addCmd(rl);
+		break;
+
+	case 'list':
+		cmds.listCmd(rl);
+		break;	
+
+	case 'show':
+		cmds.showCmd(rl, args[1]);
+		break;
+
+
+	case 'test':
+		cmds.testCmd(rl, args[1]);
+		break;
+
+
+	case 'play':
+	case 'p':
+		cmds.playCmd(rl);
+		break;
+
+
+	case 'delete':
+		cmds.deleteCmd(rl, args[1]);
+		break;
+
+	case 'edit':
+		cmds.editCmd(rl, args[1]);
+		break;
+
+	case 'credits':
+		cmds.creditsCmd(rl);
+		break;
+
+	default:
+			log(`Say what? I might have heard '${colorize(cmd, 'red')}'`);
+			log(`Use ${colorize('help','green')} para ver todos los comandos disponibles.`);
+			rl.prompt();
+			break;
+		}
+	
+}) 
+.on('close', () => {
+		log('¡¡Hasta la próxima!!');
+		process.exit(0);
+});
+
--- out.js
+++ out.js
@@ -1,45 +1,33 @@
-const chalk = require ('chalk');
+
 const figlet = require('figlet');
+const chalk = require('chalk');
 
-
-/** Dar color a un string
- *
- */
-
-const colorize = (msg, color) =>{
-    if  ( typeof color !== "undefined"){
-        msg = chalk[color].bold(msg);
-    }
-    return msg;
+const colorize = (msg, color) => {
+	if(typeof color !== "undefined") {
+		msg = chalk[color].bold(msg);
+	}
+	return msg;
 };
 
-/**
- * Escribe mensaje de log
- */
-
-const log = (msg, color) =>{
-    console.log(colorize(msg, color));
+const log = (msg, color) => {
+	console.log(colorize(msg,color));
 };
 
-/**
- * Escribe mensaje de log grande
- */
+const biglog = (msg, color) => {
 
-const biglog = (msg,color)=>{
-    log(figlet.textSync(msg, {horizontalLayaout: 'full'}), color);
+	log(figlet.textSync(msg, { horizontalLayout: 'full'}), color);
+
 };
 
-/**
- * Escribe mensaje de error
- *
- */
+const errorlog = (emsg) => {
 
-const errorlog = (emsg) =>{
-    console.log(`${colorize("Error", "red")}: ${colorize(colorize(emsg, "red"), "bgYellowBright")}`);
+	log(`${colorize("Error","red")}: ${colorize(colorize(emsg, "red"), "bgYellowBright")}`);
+
 };
-exports= module.exports = {
-    colorize,
-    log,
-    biglog,
-    errorlog
+
+exports = module.exports = {
+	colorize,
+	log,
+	biglog,
+	errorlog
 };
